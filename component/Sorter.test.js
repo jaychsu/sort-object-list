61846b7b983dfbb0c701fa169140f9eba7cb8ddb
Jaych Su <hi@jaych.su>
2017-08-22 20:20:24 +0800

    Add test for component `Sorter`

---
+++ component/Sorter.test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { shallow } from 'enzyme'
+import { expect } from 'chai'
+
+import Sorter, { SORT_BY } from './Sorter'
+
+describe('<Sorter />', () => {
+  const wrapper = shallow(
+    <Sorter>
+      <Text order={3}>B</Text>
+      <Text order={0}>A</Text>
+      <Text>C</Text>
+    </Sorter>
+  )
+
+  it('should render stuff', () => {
+    expect(wrapper.length).to.equal(1)
+    expect(wrapper.children().length).to.equal(3)
+  })
+
+  it('should sort children by its `order` prop', () => {
+    expect(wrapper.childAt(0).props().children).to.equal('A')
+    expect(wrapper.childAt(1).props().children).to.equal('B')
+    expect(wrapper.childAt(2).props().children).to.equal('C')
+  })
+})
